bf35906c0936628d446f63605d19def8130ec9e4
Danilo Zagato <daniloti.zagato@gmail.com>
2021-05-08 16:24:31 +0100

    Add suport java 16

--- src/main/java/br/com/zagato/cadastro_pessoa/CadastroPessoaApplication.java
+++ src/main/java/br/com/zagato/cadastro_pessoa/CadastroPessoaApplication.java
@@ -1,11 +1,8 @@
 package br.com.zagato.cadastro_pessoa;
 
-import br.com.zagato.cadastro_pessoa.resource.utils.MyMessageInterpolator;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.validation.Validation;
 
 @SpringBootApplication
 public class CadastroPessoaApplication implements CommandLineRunner {
